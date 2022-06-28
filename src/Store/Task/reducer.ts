784707f2d6a99c59e16b0939067e7884d9a56789
liaol1 <li.liao@datareachable.com>
2022-06-28 13:11:04 +0800

    updateStore-2022.6.28

--- src/Store/Task/reducer.ts
+++ src/Store/Task/reducer.ts
@@ -5,8 +5,8 @@
  * @lastModify li.liao 2022-6-27
  */
 
-import * as types from "./types";
-import * as TaskConstants from "../../Constants/TaskConstants";
+import * as types from "./actionTypes";
+import { ACTION_TYPE } from "./constants";
 
 const initialState: types.TaskReducer = {
   taskList: ["元素1", "元素2", "元素3"],
@@ -19,17 +19,17 @@ const TaskReducer = (
   const { type, payload } = action;
 
   switch (type) {
-    case TaskConstants.ACTION_TYPE.CREATE_NEW_TASK:
+    case ACTION_TYPE.CREATE_NEW_TASK:
       state.taskList.push(payload);
       state.taskList = [...state.taskList];
       return { ...state };
 
-    case TaskConstants.ACTION_TYPE.DELECT_TASK:
+    case ACTION_TYPE.DELECT_TASK:
       state.taskList.splice(payload, 1);
       state.taskList = [...state.taskList];
       return { ...state };
 
-    case TaskConstants.ACTION_TYPE.UPDATE_TASK:
+    case ACTION_TYPE.UPDATE_TASK:
       const { index, item } = payload;
       state.taskList[index] = item;
       state.taskList = [...state.taskList];
